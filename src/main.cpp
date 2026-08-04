b67d55f265c70596ed85debf0975eda80e1a8b8e
Zhangjun <Zhang.jun@newdimchina.com>
2026-08-04 15:51:03 +0800

    补充了vec3的数据结构定义；

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@ int main() {
     out << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++) {
+        std::cout << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
             auto r = double(i) / (image_width - 1);
             auto g = double(j) / (image_height - 1);
