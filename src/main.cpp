635ee1253f806e1de2b9c1cb61483ef2cf83c247
Zhangjun <Zhang.jun@newdimchina.com>
2026-08-04 16:00:41 +0800

    引入写出颜色的方法；

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <iostream>
 
+#include "color.h"
+
 int main() {
     // Image
 
@@ -20,15 +22,9 @@ int main() {
     for (int j = 0; j < image_height; j++) {
         std::cout << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++) {
-            auto r = double(i) / (image_width - 1);
-            auto g = double(j) / (image_height - 1);
-            auto b = 0.0;
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.0);
 
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            out << ir << ' ' << ig << ' ' << ib << '\n';
+            write_color(out, pixel_color);
         }
     }
 
