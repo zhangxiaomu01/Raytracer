97cdc753482d79419fc9302730e16f59e61b44be
Zhangjun <Zhang.jun@newdimchina.com>
2026-08-06 15:46:28 +0800

    Updates normal calculation. We record front face during the process.

--- src/objects/SphereShape.h
+++ src/objects/SphereShape.h
@@ -6,10 +6,10 @@
 
 class SphereShape : public Hittable {
 public:
-    SphereShape(point3 center, double radius) : m_center(center), m_radius(std::fmax(0,radius)) {}
+    SphereShape(Point3 center, double radius) : m_center(center), m_radius(std::fmax(0,radius)) {}
 
     bool Hit(const Ray& ray, double ray_tmin, double ray_tmax, HitRecord& hit_record) const override {
-        vec3 oc = ray.Origin() - m_center;
+        Vec3 oc = ray.Origin() - m_center;
         auto a = ray.Direction().length_squared();
         auto h = dot(oc, ray.Direction()); // h = b / -2
         auto c = dot(oc, oc) - m_radius * m_radius;
@@ -30,13 +30,14 @@ public:
         }
 
         hit_record.point = ray.At(t);
-        hit_record.normal = (hit_record.point - m_center) / m_radius;
         hit_record.t = t;
+        Vec3 outNormal = (hit_record.point - m_center) / m_radius;
+        hit_record.SetFaceNormal(ray, outNormal);
         return true;
     }
 
 private:
-    point3 m_center;
+    Point3 m_center;
     double m_radius;
 };
 
