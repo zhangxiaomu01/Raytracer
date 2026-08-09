5b346fc87eddd0aa121d2c799bb6845a2eb1e2ed
Jun Zhang <zhangxiaomu01@gmail.com>
2026-08-09 09:04:00 +0800

    添加Gamma 矫正

--- src/utility/Color.h
+++ src/utility/Color.h
@@ -8,12 +8,19 @@
 
 using Color = Vec3;
 
+// Convert linear color to gamma color
+inline Color LinearToGamma(const Color& linear_color) {
+    const double inverseGamma = 1.0 / 2.2;
+    return Vec3(pow(linear_color.x(), inverseGamma), pow(linear_color.y(), inverseGamma), pow(linear_color.z(), inverseGamma));
+}
+
 inline void WriteColor(std::ofstream& out, const Color& pixel_color) {
     static const Interval validColorRange(0.0, 0.999);
 
-    auto r = pixel_color.x();
-    auto g = pixel_color.y();
-    auto b = pixel_color.z();
+    auto gammaColor = LinearToGamma(pixel_color);
+    auto r = gammaColor.x();
+    auto g = gammaColor.y();
+    auto b = gammaColor.z();
 
     // Translate the [0,1] component values to the byte range [0,255].
     int rbyte = int(256 * validColorRange.Clamp(r));
