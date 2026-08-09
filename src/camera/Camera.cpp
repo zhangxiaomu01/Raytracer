54f7b605fa9070a130d480b405926d8984ef2ff7
Jun Zhang <zhangxiaomu01@gmail.com>
2026-08-09 08:57:54 +0800

    添加lambert反射，添加最近Interval的阈值，防止shadow acne

--- src/camera/Camera.cpp
+++ src/camera/Camera.cpp
@@ -43,8 +43,14 @@ Color Camera::RayColor(const Ray& r, const HittableObjects& scene, int depth) {
 
     // Get the color of the pixel
     HitRecord record;
-    if (scene.Hit(r, Interval(0.0, G_INFINITY), record)) {
-        Ray outRay = GetOutRay(record.point, record.normal);
+    // 0.00001 is the minimum distance to avoid shadow acne
+    if (scene.Hit(r, Interval(0.00001, G_INFINITY), record)) {
+        Vec3 outDir = record.normal + Vec3::RandomUnitVector();
+        if (outDir.length_squared() < 0.0001) { // Avoid reflection to the same direction
+            outDir = record.normal;
+        }
+        outDir = unit_vector(outDir);
+        Ray outRay = Ray(record.point, outDir);
 
         return 0.5 * RayColor(outRay, scene, depth - 1);
     }
